d4343ab80c4933a51fa99acd5855f6d311186a4c
hidayaat <m.hidayat0524@gmail.com>
2021-10-04 01:18:24 +0800

    menambahkan fitur multibahasa

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get(
+    '/localization/{language}',
+    App\Http\Controllers\LocalizationController::class
+)->name('localization.switch');
 
 Route::get('/', function () {
     return view('welcome');
