6c9a912e1d0e668d565fa4f068d859e8d5594b27
hidayaat <m.hidayat0524@gmail.com>
2021-10-07 01:14:09 +0800

    menampilkan data tags

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -2,46 +2,73 @@
 
 @section('title')
     Halaman Tag
-@endsection 
+@endsection
 
 @section('breadcrumbs')
-    {{Breadcrumbs::render('tags')}}
+    {{ Breadcrumbs::render('tags') }}
 @endsection
 
 @section('content')
     <!-- section:content -->
-<div class="row">
-    <div class="col-md-12">
-       <div class="card">
-          <div class="card-header">
-            <div class="row">
-                <div class="col-md-6">
-                   <form action="" method="GET">
-                      <div class="input-group">
-                         <input name="keyword" type="search" class="form-control" placeholder="Cari tag">
-                         <div class="input-group-append">
-                            <button class="btn btn-primary" type="submit">
-                               <i class="fas fa-search"></i>
-                            </button>
-                         </div>
-                      </div>
-                   </form>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <form action="" method="GET">
+                                <div class="input-group">
+                                    <input name="keyword" type="search" class="form-control" placeholder="Cari tag">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-primary" type="submit">
+                                            <i class="fas fa-search"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-md-6">
+                            <a href="#" class="btn btn-primary float-right" role="button">
+                                Add new
+                                <i class="fas fa-plus-square"></i>
+                            </a>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-6">
-                   <a href="#" class="btn btn-primary float-right" role="button">
-                      Add new
-                      <i class="fas fa-plus-square"></i>
-                   </a>
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        @if (count($tags))
+                            @foreach ($tags as $tag)
+                                <li
+                                    class="list-group-item list-group-item-action d-flex justify-content-between align-items-center pr-0">
+                                    <label class="mt-auto mb-auto">
+                                        {{ $tag->title }}
+                                    </label>
+                                    <div>
+                                        <!-- edit -->
+                                        <a class="btn btn-sm btn-info" role="button">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
+                                        <!-- delete -->
+                                        <form class="d-inline" action="" method="POST">
+                                            <button type="submit" class="btn btn-sm btn-danger">
+                                                <i class="fas fa-trash"></i>
+                                            </button>
+                                        </form>
+                                    </div>
+                                </li>
+                            @endforeach
+                        @else
+                            <p>
+                                <strong>
+                                    Tidak ada data
+                                </strong>
+                            </p>
+                        @endif
+                    </ul>
                 </div>
-             </div>
-          </div>
-          <div class="card-body">
-             <ul class="list-group list-group-flush">
-                <!-- list tag -->
-             </ul>
-          </div>
-       </div>
+            </div>
+        </div>
     </div>
- </div>
- 
+
 @endsection
